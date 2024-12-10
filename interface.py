f3df8e7589ab7b5d3dcdbb030e8888251f31471a
GPSIX <eric.ji1222@gmail.com>
2024-12-10 13:26:42 -0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -24,7 +24,7 @@ from spotipy_api_oauth import (
     fetch_user_profile)
 
 
-class MainInterface:
+class MaintainInterface:
     def __init__(self, root, token_manager, get_user_credentials, App):
         self.root = root
         self.root.title("MySpotify 1.0 Beta")
