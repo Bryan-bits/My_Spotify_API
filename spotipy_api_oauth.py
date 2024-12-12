d0defc827f2854657d15fe49d3e3a99f8b2e0aad
JoshLiu111 <joshua1lxy@gmail.com>
2024-12-11 17:19:10 -0800

    Update spotipy_api_oauth.py

--- spotipy_api_oauth.py
+++ spotipy_api_oauth.py
@@ -36,30 +36,7 @@ SCOPES = (
     "user-read-playback-state "
 )
 
-#get token by sending id and secrets following Spotify authorization Code Flow
-# def get_token(client_id, client_secret):
-#     auth_string = client_id + ":" + client_secret
-#     auth_bytes = auth_string.encode("utf-8")
-#     auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
-#     #
-#     url = "https://accounts.spotify.com/api/token"
-#     headers = {
-#         "Authorization": "Basic " + auth_base64,
-#         "Content-Type": "application/x-www-form-urlencoded"
-#     }
 
-#     data = {"grant_type": "client_credentials"}
-
-#     result = post(url, headers=headers, data=data)
-#     #recevei the json data
-#     json_result = json.loads(result.content)
-#     print(json_result)
-#     try:
-#         token = json_result["access_token"]
-#         os.environ["ACCESS_TOKEN"] = token
-#         return token
-#     except KeyError:
-#         return None
 
 def get_auth_header(token):
     return {"Authorization": "Bearer " + token}
